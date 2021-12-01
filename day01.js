ea3aea305cdcbca329c8da9affcdf482c32a992a
Francois Jacquier <fjacquier@gmail.com>
2021-12-01 16:02:10 +0100

    day 1 code

--- day01.js
+++ day01.js
@@ -1,1 +1,8 @@
+const fs = require('fs');
 
+const data = fs.readFileSync('input01', 'utf8');
+// if you omit mapping to Number, you'll got wrong results (missing one element)
+let output = data.split(/\n/).map(Number);
+
+const increases = output.filter((_, index) => output[index + 1] > output[index]);
+console.log(increases.length);
