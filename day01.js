c41bacedba8ac98f13f8b02c59acbbf5960f2ca6
Francois Jacquier <fjacquier@gmail.com>
2021-12-01 16:06:05 +0100

    part 2

--- day01.js
+++ day01.js
@@ -4,5 +4,12 @@ const data = fs.readFileSync('input01', 'utf8');
 // if you omit mapping to Number, you'll got wrong results (missing one element)
 let output = data.split(/\n/).map(Number);
 
-const increases = output.filter((_, index) => output[index + 1] > output[index]);
-console.log(increases.length);
+const singleIncrease = output.filter((_, index) => output[index + 1] > output[index]);
+console.log(singleIncrease.length);
+
+const sumsIncreases = output.filter((_, idx) => {
+  const sum = output[idx] + output[idx + 1] + output[idx + 2];
+  const nextSum = output[idx + 1] + output[idx + 2] + output[idx + 3];
+  return nextSum > sum;
+});
+console.log(sumsIncreases.length);
